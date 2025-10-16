7862babec7f73ffdf653ba99a8d21157feef5fe4
rahul <rm3394691>
2025-10-16 16:28:47 +0530

    seconndlar

--- secondlargest.js
+++ secondlargest.js
@@ -1,1 +1,14 @@
-let arr = [10,20,4,556,4891,1,12,46]
+function seconfdlar(arr){
+    let largest =-1, secondlargest = -1;
+    for (let i=0;i<arr.length;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    for (let i=0;i<arr.length;i++){
+        if(arr[i]>secondlargest && arr[i] !== largest)
+            secondlargest=arr[i];
+    }
+    return secondlargest;
+}
+console.log(seconfdlar([2,3,5,6,6,5]));
